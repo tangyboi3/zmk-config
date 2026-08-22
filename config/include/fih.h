f08ad26681411a00fb32b83264970057fa3f6c33
tangy <142194836+tangyboi3@users.noreply.github.com>
2026-08-22 01:33:47 -0700

    add fih-keeb

---
+++ config/include/fih.h
@@ -0,0 +1,73 @@
+/* This file was copied and then modified from https://github.com/urob/zmk-helpers/blob/main/include/zmk-helpers/key-labels/42.h
+ * LICENSE for that repo is inlined below.
+ *
+ * MIT License
+ *
+ * Copyright (c) 2023 Robert U
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*                                      30 KEY MATRIX / LAYOUT MAPPING
+      ╭────────────────┬────────────────╮         ╭─────────────────┬─────────────────╮
+  ╭───╯  0   1   2   3 │  4   5   6   7 ╰───╮ ╭───╯ LT3 LT2 LT1 LT0 │ RT0 RT1 RT2 RT3 ╰───╮
+  │  8   9  10  11  12 │ 13  14  15  16  17 │ │ LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 │
+  │ 18  19  20  21     │     22  23  24  25 │ │ LB4 LB3 LB2 LB1     │     RB1 RB2 RB3 RB4 │
+  ╰───────────╮ 26  27 │ 28  29 ╭───────────╯ ╰───────────╮ LH1 LH0 │ RH0 RH1 ╭───────────╯
+              ╰────────┴────────╯                         ╰─────────┴─────────╯             */
+
+#pragma once
+
+#define LT0  3  // left-top row
+#define LT1  2
+#define LT2  1
+#define LT3  0
+
+#define RT0  4  // right-top row
+#define RT1  5
+#define RT2  6
+#define RT3  7
+
+#define LM0 12  // left-middle row
+#define LM1 11
+#define LM2 10
+#define LM3 9
+#define LM4 8
+
+#define RM0 13  // right-middle row
+#define RM1 14
+#define RM2 15
+#define RM3 16
+#define RM4 17
+
+#define LB1 21  // left-bottom row
+#define LB2 20
+#define LB3 19
+#define LB4 18
+
+#define RB1 22  // right-bottom row
+#define RB2 23
+#define RB3 24
+#define RB4 25
+
+#define LH0 27  // left thumb keys
+#define LH1 26
+
+#define RH0 28  // right thumb keys
+#define RH1 29
