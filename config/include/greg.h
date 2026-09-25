46e2f2e4a39303be0c5b4c962d522a88941b8246
tangy <142194836+tangyboi3@users.noreply.github.com>
2026-09-24 21:27:13 -0700

    unified keymap

--- config/include/greg.h
+++ config/include/greg.h
@@ -81,3 +81,14 @@
 #define RH0 37  // right thumb keys
 #define RH1 38
 #define RH2 39
+
+#define LAYER( \
+        k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, \
+        k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, \
+        k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, \
+                  k30, k31, k32, k33, k34, k35            \
+) \
+ &none  k00  k01  k02  k03  k04  k05  k06  k07  k08  k09  &none \
+ &none  k10  k11  k12  k13  k14  k15  k16  k17  k18  k19  &none \
+        k20  k21  k22  k23  k32  k25  k26  k27  k28  k29  \
+           &none  k31  k32  k33  k34  &none
